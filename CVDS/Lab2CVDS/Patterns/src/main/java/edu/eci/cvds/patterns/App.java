469e823bfd6db1eeedf701f3d8b57eafc3d019b3
Pokecris200 <cristian.forero-mail.escuelaing.edu.co>
2021-08-25 12:52:20 -0500

    Proyecto Maven

---
+++ CVDS/Lab2CVDS/Patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -0,0 +1,25 @@
+package edu.eci.cvds.patterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	if(args.length == 0)
+    	{
+    		System.out.println( "Hello World!" );
+    	}
+    	else
+    	{
+    		String sPrint = "";
+    		for(String s: args)
+    		{
+    			sPrint = sPrint.concat(s).concat(" ");
+    		}
+    		System.out.println( sPrint );
+    	}
+    }
+}
